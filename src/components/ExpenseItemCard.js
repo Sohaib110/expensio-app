948d0d0d27c0df01bdcad3a70f1624dbee41b8f8
Sohaib110 <csscientistsohaib@gmail.com>
2025-10-09 16:54:01 +0500

    empty list card

--- src/components/ExpenseItemCard.js
+++ src/components/ExpenseItemCard.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'; 
 
 const ExpenseItemCard = ({item}) => {
   return (
-    <View>
-      <Text>ExpenseItemCard</Text>
+    <View style={tw`bg-white rounded-2xl p-4 mx-5 mb-3 flex-row justify-between items-center shadow-sm`}>
+      
     </View>
   )
 }
