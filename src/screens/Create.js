26ea3d243603ee9e8cb6349c90536b7123300c48
Sohaib110 <csscientistsohaib@gmail.com>
2025-10-11 00:36:11 +0500

    create

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,ScrollView, TextInput } from 'react-native'
+import tw from 'twrnc';
 import React from 'react'
 
 const Create = () => {
   return (
     <View>
-      <Text>Create</Text>
+      <ScrollView contentContainerStyle={tw`p-6`}>
+        <Text style={tw`text-3xl font-bold text-black`}>Add new expense</Text>
+        <Text style={tw`text-base text-gray-500 mt-2 mb-8`}>Enter the details of your expense to help you track your spending.</Text>
+        <View style={tw`mb-5 `}>
+          <Text style={tw`text-lg font-semibold text-gray-600 mb-2`}>Enter Amount</Text>
+          <TextInput
+          placeholder='$0.00' style={tw`border-2 border-blue-500 rounded-xl p-4  text-lg`}
+          value={'amount'}
+          onChangeText={(text) => console.log(text)}
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
